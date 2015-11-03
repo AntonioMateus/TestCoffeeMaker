95de43cf4479ec9de277df3f8b6e8d4d29470071
AntonioMateus <antoniocms1995@hotmail.com>
2015-11-03 13:03:35 -0200

    AllTests alterado (é o mesmo que o merge)

--- TestCoffeeMaker/test/each/usp/ach2006/eler/AllTests.java
+++ TestCoffeeMaker/test/each/usp/ach2006/eler/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ RecipeTest.class, CoffeeMakerTest.class })
+@SuiteClasses({ RecipeTest.class, CoffeeMakerTest.class, RecipeStructuralTest.class, CoffeeMakerStructuralTest.class })
 
 public class AllTests {
 
